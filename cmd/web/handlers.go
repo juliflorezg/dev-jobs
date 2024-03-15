88f1dfba64dc678fd65953a989a895036b5fafc2
Julian Florez <forezjuli08@hotmail.com>
2024-03-15 15:52:45 -0500

    add - rendering home page for "/" route

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,6 +3,8 @@ package main
 import "net/http"
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("main page, here i'll put the list of job posts"))
-}
+	//w.Write([]byte("main page, here i'll put the list of job posts"))
 
+	templateData := app.newTemplateData()
+	app.render(w, r, 200, "home.tmpl.html", templateData)
+}
