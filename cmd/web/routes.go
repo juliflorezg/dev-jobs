d06391ac345594dea17547315dd9f4dd08e2f662
Julian Florez <forezjuli08@hotmail.com>
2024-03-15 15:51:44 -0500

    add - file server usage in routes.go

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,10 +4,14 @@ import (
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/juliflorezg/dev-jobs/ui"
 )
 
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
+
+	fileServer := http.FileServer(http.FS(ui.Files))
+	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 
 	router.HandlerFunc(http.MethodGet, "/", app.home)
 
