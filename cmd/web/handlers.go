c724a88afa2f8ddaddb3242232193855483a5d85
Julian Florez <forezjuli08@hotmail.com>
2024-03-21 17:33:06 -0500

    add - usage of Latest() method for getting jobposts from MySQL DB

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,14 @@ import "net/http"
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	//w.Write([]byte("main page, here i'll put the list of job posts"))
 
+	jobposts, err := app.jobPosts.Latest()
+
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
 	templateData := app.newTemplateData()
+	templateData.JobPosts = jobposts
 	app.render(w, r, 200, "home.tmpl.html", templateData)
 }
