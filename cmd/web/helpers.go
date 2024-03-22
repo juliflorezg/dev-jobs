9eebfb8ebfbd9fb99cf9c6d3bd1b3a5fd2e2bd42
Julian Florez <forezjuli08@hotmail.com>
2024-03-22 17:40:51 -0500

    add - clientError & decodeForm app helpers

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"runtime/debug"
 	"time"
+
+	"github.com/go-playground/form/v4"
 )
 
 func ReadUserIP(r *http.Request) string {
@@ -18,6 +21,13 @@ func ReadUserIP(r *http.Request) string {
 		IPAddress = r.RemoteAddr
 	}
 	return IPAddress
+}
+
+// The clientError helper sends a specific status code and corresponding description
+// to the user. We'll use this to send responses like 400 "Bad
+// Request" when there's a problem with the request that the user sent
+func (app *application) clientError(w http.ResponseWriter, statusCode int) {
+	http.Error(w, http.StatusText(statusCode), statusCode)
 }
 
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
@@ -57,4 +67,45 @@ func (app *application) newTemplateData() templateData {
 	return templateData{
 		CurrentYear: time.Now().Year(),
 	}
+}
+
+// Create a new decodePostForm() helper method. The second parameter here, dst,
+// is the target destination that we want to decode the form data into.
+func (app *application) decodeForm(w http.ResponseWriter, r *http.Request, dst any) error {
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return err
+	}
+
+	// Call Decode() on our decoder instance, passing the target destination as
+	// the first parameter
+
+	fmt.Println("in decodeForm fn, helpers.go")
+	fmt.Printf("+%v\n", r.Form)
+	fmt.Printf("+%v\n", r.PostForm)
+
+	if method := r.Method; method == "GET" {
+		err = app.formDecoder.Decode(dst, r.Form)
+	} else {
+		err = app.formDecoder.Decode(dst, r.PostForm)
+	}
+
+	if err != nil {
+		// If we try to use an invalid target destination, the Decode() method
+		// will return an error with the type *form.InvalidDecoderError.
+		// We use errors.As() to check for this and raise a panic rather than returning
+		// the error.
+
+		var invalidDecoderError *form.InvalidDecoderError
+
+		if errors.As(err, &invalidDecoderError) {
+			panic(err)
+		}
+
+		// For all other errors, we return them as normal.
+		return err
+	}
+
+	return nil
 }
