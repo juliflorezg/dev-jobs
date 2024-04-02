b230c29aa38aa6c389930081f9031c9820a6be6b
Julian Florez <forezjuli08@hotmail.com>
2024-04-02 17:32:41 -0500

    add - validations in home filter handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,12 +6,16 @@ import (
 	"net/http"
 
 	"github.com/juliflorezg/dev-jobs/internal/models"
+	"github.com/juliflorezg/dev-jobs/internal/validator"
 )
 
 type JobPostFilterForm struct {
-	Position string `form:"position"`
-	Location string `form:"location"`
-	Contract string `form:"contract"`
+	Position            string `form:"position"`
+	Location            string `form:"location"`
+	Contract            string `form:"contract"`
+	MobileMenuClasses   string `form:"mobileMenuClasses"`
+	WindowWidth         string `form:"windowWidth"`
+	validator.Validator `form:"-"`
 }
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -26,6 +30,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	templateData := app.newTemplateData()
 	templateData.JobPosts = jobposts
+	templateData.Form = JobPostFilterForm{}
 	app.render(w, r, 200, "home.tmpl.html", templateData)
 }
 
@@ -38,6 +43,30 @@ func (app *application) homeFilterJobPosts(w http.ResponseWriter, r *http.Reques
 		return
 	}
 	fmt.Printf("%+v\n", form)
+
+	err = form.ValidateFormData((validator.IsNoData(form.Position, form.Location, form.Contract)), form.MobileMenuClasses, form.WindowWidth)
+	if err != nil {
+		app.logger.Error(err.Error())
+		return
+	}
+
+	fmt.Println()
+	fmt.Println("is form valid?? ::", form.Valid())
+	fmt.Printf("form ::>%+v \n", form)
+	fmt.Println()
+
+	if !form.Valid() {
+		data := app.newTemplateData()
+		data.Form = form
+
+		fmt.Println()
+		fmt.Printf("form when error ::>%+v \n", form)
+		fmt.Println()
+
+		app.render(w, r, http.StatusUnprocessableEntity, "home.tmpl.html", data)
+
+		return
+	}
 
 	templateData := app.newTemplateData()
 	jobPosts, err := app.jobPosts.FilterPosts(form.Position, form.Location, form.Contract)
