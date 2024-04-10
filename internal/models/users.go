e1cfbed0160cde19cfc5f8d302281df2728d09a4
Julian Florez <forezjuli08@hotmail.com>
2024-04-10 17:51:18 -0500

    feat - users signup, add logic to insert method in models/users

--- internal/models/users.go
+++ internal/models/users.go
@@ -2,11 +2,16 @@ package models
 
 import (
 	"database/sql"
+	"errors"
+	"strings"
 	"time"
+
+	"github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserModelInterface interface {
-	Insert(name, email, password string) error
+	Insert(name, email, password string, userType int) error
 	Authenticate(email, password string) (int, error)
 }
 
@@ -20,18 +25,40 @@ type User struct {
 }
 
 type UserModel struct {
-  DB *sql.DB
+	DB *sql.DB
 }
 
+func (m *UserModel) Insert(name, email, password string, userType int) error {
+	// return nil
 
-func (m *UserModel) Insert(name, email, password string) error {
-return nil
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+
+	stmt := `INSERT INTO users (name, email, hashed_password, created, type) 
+  VALUES(?, ?, ?, UTC_TIMESTAMP(), ?)`
+
+	_, err = m.DB.Exec(stmt, name, email, hashedPassword, userType)
+
+	if err != nil {
+		var mySQLError *mysql.MySQLError
+		if errors.As(err, &mySQLError) {
+			if mySQLError.Number == 1062 && strings.Contains(mySQLError.Message, "users_uc_email") {
+				return ErrDuplicateEmail
+			}
+		}
+
+		return err
+	}
+
+	return nil
 }
 
 func (m *UserModel) Authenticate(email, password string) (int, error) {
-return 0, nil
+	return 0, nil
 }
 
 func (m *UserModel) Exists(id int) (bool, error) {
-return false, nil
+	return false, nil
 }
