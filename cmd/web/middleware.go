4449b4950bb2622deb5fc8255645db96d2b97f71
Julian Florez <forezjuli08@hotmail.com>
2024-04-11 18:01:40 -0500

    feat - employers signup, add headers to logRequest middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -21,13 +21,14 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		var (
-			ip     = ReadUserIP(r)
-			proto  = r.Proto
-			method = r.Method
-			uri    = r.URL.RequestURI()
+			ip      = ReadUserIP(r)
+			proto   = r.Proto
+			method  = r.Method
+			uri     = r.URL.RequestURI()
+			headers = r.Header
 		)
 
-		app.logger.Info("Received a new request ->", "ip", ip, "protocol", proto, "method", method, "uri", uri)
+		app.logger.Info("Received a new request ->", "ip", ip, "protocol", proto, "method", method, "uri", uri, "headers", headers)
 
 		next.ServeHTTP(w, r)
 	})
