331b9482f5ba6436bc3b86cb0ae6c5aaff83f6c2
Julian Florez <forezjuli08@hotmail.com>
2024-04-12 17:42:50 -0500

    feat - employers signup, add JS for interactivity with info message

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -13,36 +13,82 @@ signUpBtnMenu = document.querySelector(
   '.header-access-container__sign-up-users-list'
 )
 
-formMobileMenu.addEventListener('click', function (e) {
-  if (e.target === formMobileMenu) {
-    console.log('touched on mobile menu')
+companySignUpForm = document.querySelector('#companySignUpForm')
+
+userSignupFormInfoMessage = document.querySelector('#userSignupFormInfoMessage')
+userSignupFormInfoIcon = document.querySelector(
+  '.user-signup-form__info-container--icon-container'
+)
+
+userSignupInfoList = document.querySelector(
+  '.user-signup-form__info-container--info-list'
+)
+signupFormInfoCloseIcon = document.querySelector(
+  '.info-container__info-list--close-icon-container'
+)
+
+if (formMobileMenu) {
+  formMobileMenu.addEventListener('click', function (e) {
+    if (e.target === formMobileMenu) {
+      console.log('touched on mobile menu')
+      formMobileMenu.classList.add('hidden')
+    }
+  })
+}
+
+if (closeFormIcon) {
+  closeFormIcon.addEventListener('click', function (e) {
+    console.log('click on' + e.target)
     formMobileMenu.classList.add('hidden')
-  }
-})
+  })
+}
 
-closeFormIcon.addEventListener('click', function (e) {
-  console.log('click on' + e.target)
-  formMobileMenu.classList.add('hidden')
-})
+if (openFormMenuIcon) {
+  openFormMenuIcon.addEventListener('click', function (e) {
+    formMobileMenu.classList.remove('hidden')
+  })
+}
 
-openFormMenuIcon.addEventListener('click', function (e) {
-  formMobileMenu.classList.remove('hidden')
-})
-
+if (companySignUpForm) {
+}
 document.addEventListener('DOMContentLoaded', function (e) {
   console.log(window.innerWidth)
 
-  if (window.innerWidth >= 768) {
+  if (window.innerWidth >= 768 && formMobileMenu) {
     formMobileMenu.classList.remove('hidden')
   }
 })
 
-headerSignInBtn.addEventListener('click', function (e) {
-  signInBtnMenu.classList.toggle('hidden')
-  signUpBtnMenu.classList.add('hidden')
-})
+if (headerSignInBtn) {
+  headerSignInBtn.addEventListener('click', function (e) {
+    signInBtnMenu.classList.toggle('hidden')
+    signUpBtnMenu.classList.add('hidden')
+  })
+}
 
-headerSignUpBtn.addEventListener('click', function (e) {
-  signUpBtnMenu.classList.toggle('hidden')
-  signInBtnMenu.classList.add('hidden')
-})
+if (headerSignUpBtn) {
+  headerSignUpBtn.addEventListener('click', function (e) {
+    signUpBtnMenu.classList.toggle('hidden')
+    signInBtnMenu.classList.add('hidden')
+  })
+}
+
+
+if (userSignupFormInfoMessage) {
+  userSignupFormInfoMessage.addEventListener('click', function (e) {
+    userSignupInfoList.classList.toggle('hidden')
+  })
+}
+
+if (userSignupFormInfoIcon) {
+  userSignupFormInfoIcon.addEventListener('click', function (e) {
+    userSignupInfoList.classList.toggle('hidden')
+  })
+}
+
+if (signupFormInfoCloseIcon) {
+  signupFormInfoCloseIcon.addEventListener('click', function (e) {
+    console.log(e.target.tagName)
+    userSignupInfoList.classList.toggle('hidden')
+  })
+}
