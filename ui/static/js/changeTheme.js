a73ca93695b114ba39e70bec1ab2078e54d4aabd
Julian Florez <forezjuli08@hotmail.com>
2024-05-10 17:44:55 -0500

    feat - add JS for theme toggle

---
+++ ui/static/js/changeTheme.js
@@ -0,0 +1,34 @@
+const root = document.documentElement
+const toggleBtn = document.querySelector('.toggle-theme-container__button')
+let currentTheme = 'light' // ! GLOBAL
+
+function changeTheme(theme) {
+  if (theme === 'light') {  
+    root.style.setProperty('--bg-color', 'hsl(220, 29%, 10%)')
+    root.style.setProperty('--card-color', 'hsl(219, 29%, 14%)')
+    root.style.setProperty('--text-titles', 'hsl(0, 0%, 100%')
+    root.style.setProperty('--text-paragraph', 'hsl(214, 17%, 51%)')
+    e.target.dataset.activeTheme = 'dark'
+    
+  } else if (theme === 'dark'){
+    root.style.setProperty('--bg-color', 'hsl(210, 22%, 96%')
+    root.style.setProperty('--card-color', 'hsl(0, 0%, 100%')
+    root.style.setProperty('--text-titles', 'hsl(219, 29%, 14%')
+    root.style.setProperty('--text-paragraph', 'hsl(210, 29%, 40%)')
+    e.target.dataset.activeTheme = 'light'
+  }
+}
+
+toggleBtn.addEventListener('click', function(e) {
+  currentTheme = e.target.dataset.activeTheme
+  console.log(e.target);
+
+  console.log(e.target.dataset);
+
+  changeTheme(currentTheme)
+})
+
+document.addEventListener('DOMContentLoaded', function(){
+  changeTheme(currentTheme)
+})
+
