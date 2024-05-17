f662143a232409b8cf074cb63044f28f4b9e6367
Julian Florez <forezjuli08@hotmail.com>
2024-05-16 19:49:34 -0500

    fix - change in colors in changeTheme.js

--- ui/static/js/changeTheme.js
+++ ui/static/js/changeTheme.js
@@ -6,14 +6,14 @@ function changeTheme(theme) {
   if (theme === 'light') {  
     root.style.setProperty('--bg-color', 'hsl(220, 29%, 10%)')
     root.style.setProperty('--card-color', 'hsl(219, 29%, 14%)')
-    root.style.setProperty('--text-titles', 'hsl(0, 0%, 100%')
+    root.style.setProperty('--text-titles', 'hsl(0, 0%, 100%)')
     root.style.setProperty('--text-paragraph', 'hsl(214, 17%, 51%)')
     toggleBtn.dataset.activeTheme = 'dark'
     
   } else if (theme === 'dark'){
-    root.style.setProperty('--bg-color', 'hsl(210, 22%, 96%')
-    root.style.setProperty('--card-color', 'hsl(0, 0%, 100%')
-    root.style.setProperty('--text-titles', 'hsl(219, 29%, 14%')
+    root.style.setProperty('--bg-color', 'hsl(210, 22%, 96%)')
+    root.style.setProperty('--card-color', 'hsl(0, 0%, 100%)')
+    root.style.setProperty('--text-titles', 'hsl(219, 29%, 14%)')
     root.style.setProperty('--text-paragraph', 'hsl(210, 29%, 40%)')
     toggleBtn.dataset.activeTheme = 'light'
   }
