0dca76267d9d1e1609b505e307620545c2f27100
Julian Florez <forezjuli08@hotmail.com>
2024-06-18 20:53:02 -0500

    feat - job appliaction, add jobpost method for inserting a new job application and fixing default svg for company

--- internal/models/jobposts.go
+++ internal/models/jobposts.go
@@ -10,7 +10,7 @@ import (
 	"github.com/juliflorezg/dev-jobs/internal/validator"
 )
 
-var DefaultCompanyIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M384 96V320H64L64 96H384zM64 32C28.7 32 0 60.7 0 96V320c0 35.3 28.7 64 64 64H181.3l-10.7 32H96c-17.7 0-32 14.3-32 32s14.3 32 32 32H352c17.7 0 32-14.3 32-32s-14.3-32-32-32H277.3l-10.7-32H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm464 0c-26.5 0-48 21.5-48 48V432c0 26.5 21.5 48 48 48h64c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48H528zm16 64h32c8.8 0 16 7.2 16 16s-7.2 16-16 16H544c-8.8 0-16-7.2-16-16s7.2-16 16-16zm-16 80c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16s-7.2 16-16 16H544c-8.8 0-16-7.2-16-16zm32 160a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" fill="#8a8eb4"/></svg>`
+var DefaultCompanyIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" width="50" height="50"><path d="M384 96V320H64L64 96H384zM64 32C28.7 32 0 60.7 0 96V320c0 35.3 28.7 64 64 64H181.3l-10.7 32H96c-17.7 0-32 14.3-32 32s14.3 32 32 32H352c17.7 0 32-14.3 32-32s-14.3-32-32-32H277.3l-10.7-32H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm464 0c-26.5 0-48 21.5-48 48V432c0 26.5 21.5 48 48 48h64c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48H528zm16 64h32c8.8 0 16 7.2 16 16s-7.2 16-16 16H544c-8.8 0-16-7.2-16-16s7.2-16 16-16zm-16 80c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16s-7.2 16-16 16H544c-8.8 0-16-7.2-16-16zm32 160a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" fill="#8a8eb4"/></svg>`
 
 var DefaultCompanyBgColor = `hsl(234, 100%, 94%)`
 
@@ -22,6 +22,7 @@ type JobPostModelInterface interface {
 	InsertJobPost(companyUserID int, jobPostData CreateJopPostFields) error
 	DeleteJobPost(jobPostID int) error
 	EditJobPost(jobPostData EditJopPostFields) error
+	InsertJobApplication(jobPostID, userID int, cvName, cvPath, coverLetter string) error
 }
 
 type JobPost struct {
@@ -49,10 +50,21 @@ type Requirements struct {
 	RequirementsDescription string
 	RequirementsList        []string
 }
+
 type Role struct {
 	RoleID          int
 	RoleDescription string
 	RoleList        []string
+}
+
+type JobApplication struct {
+	ID          int
+	jobPostID   int
+	userID      int
+	cvName      string
+	cvPath      string
+	coverLetter string
+	appliedAt   time.Time
 }
 
 type JobPostModel struct {
@@ -416,6 +428,63 @@ func (jp *JobPostModel) EditJobPost(jobPostData EditJopPostFields) error {
 	fmt.Println()
 	fmt.Printf("rows affected for role: %v", rows)
 	fmt.Println()
+
+	return nil
+}
+
+func (jp *JobPostModel) InsertJobApplication(jobPostID, userID int, cvName, cvPath, coverLetter string) error {
+
+	fmt.Printf("jp id %v: \n", jobPostID)
+	fmt.Printf("user id %v: \n", userID)
+	fmt.Printf("cv name id %v: \n", cvName)
+	fmt.Printf("cv path id %v: \n", cvPath)
+	fmt.Printf("coverletter id %v: \n", coverLetter)
+
+	//~~ first query the job_applications table to see if there is already a job application for this jp and user
+	var err error
+	var result sql.Result
+	stmt1 := `SELECT jobpost_id, user_id FROM job_applications WHERE user_id = ?`
+	var jobApplication JobApplication
+	err = jp.DB.QueryRow(stmt1, userID).Scan(&jobApplication.jobPostID, &jobApplication.userID)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			fmt.Println(err.Error())
+		} else {
+			return err
+		}
+	}
+
+	if jobApplication.jobPostID == jobPostID && jobApplication.userID == userID {
+		fmt.Println()
+		fmt.Println("user already has applied to this jobpost")
+		fmt.Println()
+		return ErrUseAlreadyApplied
+	}
+	// job_applications
+	stmt2 := `INSERT INTO job_applications(jobpost_id, user_id, cv_name, cv_path, applied_at, cover_letter)
+	VALUES (?, ?, ?, ?, UTC_TIMESTAMP(), ?)`
+
+	if coverLetter == "" {
+		fmt.Println("---- no cover letter ----")
+		result, err = jp.DB.Exec(stmt2, jobPostID, userID, cvName, cvPath, "none")
+	} else {
+		fmt.Println("---- with cover letter ----")
+		result, err = jp.DB.Exec(stmt2, jobPostID, userID, cvName, cvPath, coverLetter)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	// Get the last inserted ID
+	lastInsertID, err := result.LastInsertId()
+	if err != nil {
+		fmt.Println("Error getting last insert ID:", err)
+		return err
+	}
+
+	fmt.Printf("Last insert ID: %d\n", lastInsertID)
 
 	return nil
 }
